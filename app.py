93ffe87ea2dca39975102e8156ac4f93cf1cdf35
Wincel Anthony Generosa <113222155+WincelAnthony@users.noreply.github.com>
2023-07-31 09:45:12 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+import processor
+import time
+
+
+app = Flask(__name__)
+@app.route('/chatbot', methods=["GET", "POST"])
+def chatbotResponse():
+
+    if request.method == 'POST':
+        the_question = request.form['question']
+
+        response = processor.chatbot_response(the_question)
+
+    return jsonify({"response": response })
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',)
